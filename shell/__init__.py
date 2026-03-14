d2c3b631ed6e818ee76baa18b01bf9fa8c83daf1
Chintan Patel <216989679+Chintanpatel24@users.noreply.github.com>
2026-03-14 15:14:52 +0530

    Create __init__.py

---
+++ shell/__init__.py
@@ -0,0 +1,20 @@
+from .canvas import (
+    paint, bold, dim, green, red, yellow, cyan, magenta, grey,
+    teal, violet, orange, white,
+    rule, double_rule, blank, heading, section_open, section_close,
+    kv, bullet, ok, warn, err, info_line,
+    progress, progress_done, spin_tick,
+    format_menu, ask_file, ask_int, ask_str,
+    LOGO, W,
+)
+
+__all__ = [
+    "paint", "bold", "dim", "green", "red", "yellow", "cyan",
+    "magenta", "grey", "teal", "violet", "orange", "white",
+    "rule", "double_rule", "blank", "heading",
+    "section_open", "section_close",
+    "kv", "bullet", "ok", "warn", "err", "info_line",
+    "progress", "progress_done", "spin_tick",
+    "format_menu", "ask_file", "ask_int", "ask_str",
+    "LOGO", "W",
+]
